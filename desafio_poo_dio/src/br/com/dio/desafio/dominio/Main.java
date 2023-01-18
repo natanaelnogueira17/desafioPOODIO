d543ddbb2c4e18c3403ac9a4191abbab41f9ac11
natanael nogueira <natanaelnogueira019@gmail.com>
2023-01-18 14:57:02 -0300

    criando classes mentoria e curso

---
+++ desafio_poo_dio/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,20 @@
+package br.com.dio.desafio.dominio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("excelente curso de java com Camila");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso JS");
+		curso2.setDescricao("excelente curso de JS com Camila");
+		curso2.setCargaHoraria(6);
+		
+		
+
+	}
+
+}
