119cc9e88b68dc055094fa026e295b80beb156c8
natanael nogueira <natanaelnogueira019@gmail.com>
2023-01-18 15:27:46 -0300

    melhorando o codigo com a classe conteudo

---
+++ desafio_poo_dio/src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,70 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+	
+	private String nome;
+	private Set<Conteudo> conteudoInscritos = new LinkedHashSet<>();
+	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<Conteudo>();
+	
+	public void inscreverBootcamp(Bootcamp bootcamp) {
+		
+	}
+	
+	public void progredir() {
+		
+	}
+	
+	public void calcularTotalXP() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Conteudo> getConteudoInscritos() {
+		return conteudoInscritos;
+	}
+
+	public void setConteudoInscritos(Set<Conteudo> conteudoInscritos) {
+		this.conteudoInscritos = conteudoInscritos;
+	}
+
+	public Set<Conteudo> getConteudosConcluidos() {
+		return conteudosConcluidos;
+	}
+
+	public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+		this.conteudosConcluidos = conteudosConcluidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conteudoInscritos, conteudosConcluidos, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dev other = (Dev) obj;
+		return Objects.equals(conteudoInscritos, other.conteudoInscritos)
+				&& Objects.equals(conteudosConcluidos, other.conteudosConcluidos) && Objects.equals(nome, other.nome);
+	}
+	
+	
+	
+
+}
